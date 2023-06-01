01a87e2bc504b36967516af97e6a797d973be20c
Allyson Wilson <allysonwilson@allysons-mbp.lan>
2023-06-01 14:55:45 -0500

    Computer's move algorithm

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -6,6 +6,15 @@ public class RockPaperScissor {
 
         while(true) {
             //1. Random Computer Move
+        String [] availableMoves = {"Rock", "Paper", "Scissors"};
+        String computerMove = availableMoves[new Random().nextInt(availableMoves.length)];
+
+        System.out.println("Computer has chosen its move.");
+            System.out.println();
+        System.out.println("Now it's your turn to choose! Good luck!");
+            System.out.println();
+
+            //2. Player's Move
         }
     }
 }
